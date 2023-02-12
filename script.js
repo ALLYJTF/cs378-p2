f342088b5d013ffaffec092b6f0a561997aa76f7
ALLYJTF <arialuocollege@gmail.com>
2023-02-11 23:13:20 -0600

    functional subtotal change

--- script.js
+++ script.js
@@ -17,29 +17,68 @@ function change_text() {
     const categoty = id.substring(1);
     
     if (action === "+") {
-        add(categoty)
+        add_item(categoty)
 
     }
     if (action ===  "-") {
-        subtract(categoty)
+        del_item(categoty)
     }
 }
 
 
 
-function add(category)
-{
-  var quantity = parseInt(document.getElementById(category).innerHTML);
-  var newQuant = quantity + 1;
-  document.getElementById(category).innerHTML = newQuant;
+function add_item(category) {
+
+    let quantity = parseInt(document.getElementById(category).innerHTML);
+
+    // Changing the COUNTER NUMBER
+    let newQuant = quantity + 1;
+    document.getElementById(category).innerHTML = newQuant;
+
+    // Changing SUBTOTAL
+    let subtotal_str = document.getElementById("subtotal").innerHTML.substring(11);
+    let price_str = document.getElementById(category + "price").innerHTML.substring(1);    
+    let subtotal = parseInt(subtotal_str);
+    let item_price = parseInt(price_str);
+    console.log("subtotal: " + "     '" + document.getElementById("subtotal").innerHTML + "'       " + "price " + price_str);
+    console.log(subtotal + "      " + item_price + "\n");
+    console.log(subtotal + item_price);
+    let sum = subtotal + item_price;
+    let sum_str = "Subtotal: $" + sum;
+    document.getElementById("subtotal").innerHTML = sum_str;
+
 }
 
-function subtract(category)
+function del_item(category)
 {
   var quantity = parseInt(document.getElementById(category).innerHTML);
   if (quantity > 0) {
+
+    // Changing the COUNTER NUMBER
     var newQuant = quantity - 1;
     document.getElementById(category).innerHTML = newQuant;
+
+    // Changing SUBTOTAL
+    let subtotal_str = document.getElementById("subtotal").innerHTML.substring(11);
+    let price_str = document.getElementById(category + "price").innerHTML.substring(1);    
+    let subtotal = parseInt(subtotal_str);
+    let item_price = parseInt(price_str);
+    console.log("subtotal: " + "     '" + document.getElementById("subtotal").innerHTML + "'       " + "price " + price_str);
+    console.log(subtotal + "      " + item_price + "\n");
+    console.log(subtotal + item_price);
+    let sum = subtotal - item_price;
+    let sum_str = "Subtotal: $" + sum;
+    document.getElementById("subtotal").innerHTML = sum_str;
+
+
+
   }
+
+
+
+
+
+
+
   
 }
